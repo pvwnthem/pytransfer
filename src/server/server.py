3f7f9561a026a8d7ddfd55f6c8e568870396841a
pvwnthem <pvwn.them@gmail.com>
2023-06-24 15:48:21 -0400

    fix folder sending?

--- src/server/server.py
+++ src/server/server.py
@@ -1,4 +1,5 @@
 import socket
+import os
 
 class Server:
     def __init__(self):
@@ -6,14 +7,7 @@ class Server:
         self.host = socket.gethostname()
         self.port = 9999
 
-    def run(self):
-        self.server_socket.bind((self.host, self.port))
-        self.server_socket.listen(1)
-        print("Server is listening for incoming connections on port", self.port, "with ip", self.server_socket.getsockname()[0])
-
-        client_socket, addr = self.server_socket.accept()
-        print("Connected to:", addr)
-
+    def receive_file(self, client_socket):
         file_name_size = int.from_bytes(client_socket.recv(4), "big")
         file_name = client_socket.recv(file_name_size).decode()
 
@@ -28,6 +22,47 @@ class Server:
             print("File received successfully:", file_name)
         except IOError as e:
             print("An error occurred while receiving the file:", str(e))
+
+    def receive_folder(self, client_socket):
+        folder_name = client_socket.recv(1024).decode()
+
+        try:
+            os.mkdir(folder_name)
+        except FileExistsError:
+            pass
+
+        os.chdir(folder_name)
+
+        while True:
+            self.receive_file(client_socket)
+            client_socket.send(b"ACK")  # Send acknowledgement to client
+
+            # Check if all files have been received
+            data = client_socket.recv(1024)
+            if data == b"DONE":
+                break
+
+        os.chdir("..")
+
+        print("Folder received successfully:", folder_name)
+
+    def run(self):
+        self.server_socket.bind((self.host, self.port))
+        self.server_socket.listen(1)
+        print("Server is listening for incoming connections on port", self.port, "with IP", self.server_socket.getsockname()[0])
+
+        client_socket, addr = self.server_socket.accept()
+        print("Connected to:", addr)
+
+        while True:
+            data = client_socket.recv(1024)
+            if not data:
+                break
+
+            if data == b"FILE":
+                self.receive_file(client_socket)
+            elif data == b"FOLDER":
+                self.receive_folder(client_socket)
 
         client_socket.close()
         self.server_socket.close()
